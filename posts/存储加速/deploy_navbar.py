e46a271e0fdc2fd0c78a10b1785c3fc9b5f79dde
陆政 <lz@hztrinet.com>
2022-10-28 14:12:00 +0800

    io_uring

---
+++ posts/存储加速/deploy_navbar.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# _*_ coding:utf-8 _*_
+# 将当前目录下的 __navbar.md拷贝到 posts 里以及其各子目录里
+# TODO：各子目录需要手动拷贝
+
+import os
+
+
+root = "."
+__navbar = "_navbar.md"
+
+
+def main():
+    abs_root = os.path.abspath(root)
+    source = os.path.join(abs_root, __navbar)
+    target_root = abs_root
+    for dir in os.listdir(target_root):
+        if os.path.isdir(os.path.join(target_root, dir)):
+            cmd = 'copy "{}" "{}"'.format(source, os.path.join(target_root, dir, __navbar))
+            print(cmd)
+            os.system(cmd)
+
+
+if __name__ == "__main__":
+    main()
