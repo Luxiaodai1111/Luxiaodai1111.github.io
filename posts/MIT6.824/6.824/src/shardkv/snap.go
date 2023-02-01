021d500a665cc337a8dc083af3c84350f9435f35
陆政 <lz@hztrinet.com>
2023-02-01 12:19:29 +0800

    fix

--- posts/MIT6.824/6.824/src/shardkv/snap.go
+++ posts/MIT6.824/6.824/src/shardkv/snap.go
@@ -2,6 +2,7 @@ package shardkv
 
 import (
 	"6.824/labgob"
+	"6.824/shardctrler"
 	"bytes"
 	"fmt"
 	"strconv"
@@ -10,7 +11,7 @@ import (
 
 type DupHistorySnap map[int64]string
 
-func (kv *ShardKV) makeDupHistorySnap(dupMap map[int64]map[int64]struct{}) DupHistorySnap {
+func (kv *ShardKV) encodeDupHistory(dupMap map[int64]map[int64]struct{}) DupHistorySnap {
 	snap := make(DupHistorySnap, 0)
 	for clientId, info := range dupMap {
 		var seqs []int64
@@ -47,7 +48,7 @@ func (kv *ShardKV) makeDupHistorySnap(dupMap map[int64]map[int64]struct{}) DupHi
 	return snap
 }
 
-func (kv *ShardKV) restoreDupHistorySnap(snap DupHistorySnap) map[int64]map[int64]struct{} {
+func (kv *ShardKV) decodeDupHistory(snap DupHistorySnap) map[int64]map[int64]struct{} {
 	dupMap := make(map[int64]map[int64]struct{})
 	for clientId, info := range snap {
 		if _, ok := dupMap[clientId]; !ok {
@@ -71,15 +72,47 @@ func (kv *ShardKV) restoreDupHistorySnap(snap DupHistorySnap) map[int64]map[int6
 	return dupMap
 }
 
+type ShardStateSnap map[int]*ShardStateSimple
+
+type ShardStateSimple struct {
+	State         string
+	PrevCfgNum    int
+	CurrentCfgNum int
+}
+
+func (kv *ShardKV) encodeShardState() ShardStateSnap {
+	snap := make(ShardStateSnap)
+	for shard, info := range kv.shardState {
+		snap[shard] = &ShardStateSimple{
+			State:         info.State,
+			PrevCfgNum:    info.PrevCfg.Num,
+			CurrentCfgNum: info.CurrentCfg.Num,
+		}
+	}
+	return snap
+}
+
+func (kv *ShardKV) decodeShardState(snap ShardStateSnap) {
+	kv.shardState = make(map[int]*ShardState, shardctrler.NShards)
+	for shard, info := range snap {
+		kv.shardState[shard] = &ShardState{
+			State:      info.State,
+			PrevCfg:    &kv.configs[info.PrevCfgNum],
+			CurrentCfg: &kv.configs[info.CurrentCfgNum],
+		}
+	}
+}
+
 func (kv *ShardKV) makeSnap(applyLogIndex int) {
 	kv.DPrintf("======== snapshot %d ========", applyLogIndex)
 
 	kv.Lock("makeSnap")
 	defer kv.Unlock("makeSnap")
-	dupCommandHistorySnap := kv.makeDupHistorySnap(kv.dupCommand)
+	dupCommandHistorySnap := kv.encodeDupHistory(kv.dupCommand)
+	shardStateSnap := kv.encodeShardState()
 	w := new(bytes.Buffer)
 	e := labgob.NewEncoder(w)
-	if e.Encode(kv.db) != nil || e.Encode(kv.configs) != nil || e.Encode(kv.shardState) != nil || e.Encode(dupCommandHistorySnap) != nil {
+	if e.Encode(kv.db) != nil || e.Encode(kv.configs) != nil || e.Encode(shardStateSnap) != nil || e.Encode(dupCommandHistorySnap) != nil {
 		panic(fmt.Sprintf("[panic] encode snap error"))
 	}
 	data := w.Bytes()
@@ -92,12 +125,14 @@ func (kv *ShardKV) restoreFromSnap(snapshot []byte, snapshotIndex int) {
 	defer kv.Unlock("restoreFromSnap")
 
 	var dupCommandHistorySnap DupHistorySnap
+	var shardStateSnap ShardStateSnap
 	r := bytes.NewBuffer(snapshot)
 	d := labgob.NewDecoder(r)
-	if d.Decode(&kv.db) != nil || d.Decode(&kv.configs) != nil || d.Decode(&kv.shardState) != nil || d.Decode(&dupCommandHistorySnap) != nil {
+	if d.Decode(&kv.db) != nil || d.Decode(&kv.configs) != nil || d.Decode(&shardStateSnap) != nil || d.Decode(&dupCommandHistorySnap) != nil {
 		panic(fmt.Sprintf("[panic] decode snap error"))
 	}
-	kv.dupCommand = kv.restoreDupHistorySnap(dupCommandHistorySnap)
+	kv.dupCommand = kv.decodeDupHistory(dupCommandHistorySnap)
+	kv.decodeShardState(shardStateSnap)
 
 	// lastApplyIndex 到快照之间的修改请求一定会包含在查重哈希表里
 	// 对于读只需要让客户端重新尝试即可
